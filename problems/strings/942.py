4cd1fe8febfaedc3859116db83982184335d04e4
Baptiste Saliba <baptsal@gmail.com>
2019-12-18 15:10:27 -0500

    Finished problem 942

---
+++ problems/strings/942.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        n = len(s)
+        list = [0]*(n+1)
+        top = n
+        bottom = 0
+        for i in range(n):
+            if s[i] == 'I':
+                list[i] = bottom
+                bottom = bottom + 1
+            else:
+                list[i] = top
+                top = top - 1
+        list[n] = bottom
+        return list
+
+
+sol = Solution()
+print(sol.diStringMatch("IDID"))
+
+
+
