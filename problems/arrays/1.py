be582a814c6d1cb5f62afbedc868c858abf414b6
Baptiste Saliba <baptsal@gmail.com>
2019-12-18 16:06:00 -0500

    Finished problem 1

---
+++ problems/arrays/1.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sol = [0, 0]
+        my_map = dict()
+        for x in range(len(nums)):
+            compliment = target - nums[x]
+            if compliment in my_map:
+                sol[0] = x
+                sol[1] = my_map[compliment]
+                return sol
+            my_map[nums[x]] = x
+
+
+sol = Solution()
+print(sol.twoSum([1,3,4,2], 6))
+
