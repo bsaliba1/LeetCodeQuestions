76ec500d9f9fa70b2a657c2ed5fcb43c2bb38d43
Baptiste Saliba <baptsal@gmail.com>
2019-12-21 17:17:03 -0500

    Finished problem 206

--- problems/linked_lists/206.py
+++ problems/linked_lists/206.py
@@ -1,19 +1,60 @@
- #Definition for singly-linked list.
+# 206. Reverse Linked List
+# Task: Reverse a singly linked list.
+# Definition for singly-linked list.
 
- class ListNode:
-     def __init__(self, x):
-         self.val = x
-         self.next = None
 
-class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        print(head.val)
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-def createList(arr = [1,2,3,4,5]) -> ListNode:
+
+def createList(arr=[1, 2, 3, 4, 5]) -> ListNode:
+    if len(arr) == 0:
+        return None
     head = ListNode(arr[0])
     temp = head
-    for i in range(1,arr.len()):
+    for i in range(1, len(arr)):
         temp.next = ListNode(arr[i])
         temp = temp.next
+    return head
 
-createList([1,2,3,4,5])
+
+def printList(head: ListNode):
+    if head is None:
+        print("[]", end=' ')
+    while head is not None:
+        print(head.val, end=' ')
+        head = head.next
+    print('')
+
+
+class Solution:
+    @staticmethod
+    def reverseList(head: ListNode) -> ListNode:
+        # Check that list is more than one element
+        if head is None or head.next is None:
+            return head
+
+        double_next = head.next.next
+        current = head.next
+        prev = head
+        prev.next = None
+        while double_next is not None:
+            current.next = prev
+            prev = current
+            current = double_next
+            double_next = double_next.next
+        current.next = prev
+        return current
+
+
+# Driver
+
+print("Before: ")
+ll = createList([])
+printList(ll)
+
+print("After: ")
+rev_arr = Solution.reverseList(ll)
+printList(rev_arr)
