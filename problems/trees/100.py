01ec37fbe6b27b040d9cc2d966c02d5d5fbd961a
Baptiste Saliba <baptsal@gmail.com>
2019-12-22 00:45:49 -0500

    Finished problem 100

--- problems/trees/100.py
+++ problems/trees/100.py
@@ -1,2 +1,29 @@
 # 100. Same Tree
 # Link: https://leetcode.com/problems/same-tree/
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    @staticmethod
+    def isSameTree(x: TreeNode, y: TreeNode) -> bool:
+        if x is not None and y is None:
+            return False
+        if y is not None and x is None:
+            return False
+        if x is None and y is None:
+            return True
+        if x.val != y.val:
+            return False
+
+        if Solution().isSameTree(x.left, y.left) and Solution().isSameTree(x.right, y.right):
+            return True
+        else:
+            return False
+
+        return True
