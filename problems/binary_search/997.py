495f66f11232cc6aa0c3129ba1db04d6fee7fe6a
Baptiste Saliba <baptsal@gmail.com>
2019-12-22 19:57:13 -0500

    Refactored first solution and added directed graph solution

--- problems/binary_search/997.py
+++ problems/binary_search/997.py
@@ -7,18 +7,39 @@ from typing import List
 class Solution:
     @staticmethod
     def findJudge(N: int, trust: List[List[int]]) -> int:
-        if len(trust) != 0:
-            trust_given = [0]*N
-            trust_received = [0]*N
-            for x, y in trust:
-                trust_given[x-1] += 1
-                trust_received[y-1] += 1
-            for i in range(0, N):
-                if trust_given[i] == 0 and trust_received[i] == N-1:
-                    return i+1
-            return -1
-        else:
-            return 1
+        trust_given = [0]*(N+1)
+        trust_received = [0]*(N+1)
+        for x, y in trust:
+            trust_given[x] += 1
+            trust_received[y] += 1
+        for i in range(1, N+1):
+            if trust_given[i] == 0 and trust_received[i] == N-1:
+                return i
+        return -1
+
+    @staticmethod
+    def findJudgeGraph(N:int, trust: List[List[int]]) -> int:
+        graph = [0]*(N+1)
+        for x, y in trust:
+            graph[y] += 1
+            graph[x] -= 1
+        for i in range(1, N+1):
+            if graph[i] == N-1:
+                return i
+        return -1
 
 
+print(Solution().findJudge(1, []))
+print(Solution().findJudgeGraph(1, []))
 print(Solution().findJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))
+print(Solution().findJudgeGraph(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))
+
+def findJudge(N: int, trust: List[List[int]]) -> int:
+   graph = [0]*(N+1)
+   for x, y in trust:
+       graph[y] += 1
+       graph[x] -= 1
+   for i in range(1, N+1):
+       if graph[i] == N-1:
+           return i
+   return -1
