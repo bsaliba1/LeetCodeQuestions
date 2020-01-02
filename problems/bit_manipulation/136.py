83b99a4a2df4bda0b755648efa25ef5324d63fa9
Baptiste Saliba <baptsal@gmail.com>
2020-01-02 16:50:45 -0500

    Finished problem 136

--- problems/bit_manipulation/136.py
+++ problems/bit_manipulation/136.py
@@ -5,5 +5,31 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        ref = {}
+        for i in nums:
+            if i not in ref:
+                ref[i] = 0
+            else:
+                ref[i] += 1
+
+        for k, v in ref.items():
+            if not v:
+                return k
+
+    def singleNum(self, nums: List[int]) -> int:
+        sol = 0
+        for n in nums:
+            sol ^= n
+
+        return sol
+
+
+print(Solution().singleNum([2, 2, 1]))
+
+
+
+
+
+
 
 
