96535d561c8dcdf6a880bc7862158b0a313fd41f
Baptiste Saliba <baptsal@gmail.com>
2020-04-08 14:58:16 -0400

    Added documentation for group anagram question

--- problems/strings/49.py
+++ problems/strings/49.py
@@ -9,11 +9,12 @@ class Solution:
         word_map = dict()
         for word in strs:
             # List number of times each letter occurs in this word
-            num_letters = [0] * 26
+            num_letters = [0] * 26  # Bit vector
             for c in word:
                 num_letters[ord(c) - ord('a')] += 1  # ord gets ascii value of char
 
-            num_letters = tuple(num_letters)
+            num_letters = tuple(num_letters)  # List is unhashable so must turn list into tuple
+
             # If word_map doesn't have list for that anagram make one
             if word_map.get(num_letters, None) is None:
                 word_map[num_letters] = []
