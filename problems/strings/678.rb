8b2f7fb94582156521d9b8f9d6b1b527f08b5b0e
Baptiste Saliba <baptsal@gmail.com>
2020-07-27 00:25:51 -0400

    Added slow and fast solution to problem 678

--- problems/strings/678.rb
+++ problems/strings/678.rb
@@ -1,78 +1,76 @@
 # 678. Valid Parenthesis String
 # Link: https://leetcode.com/problems/valid-parenthesis-string/
 
-# Works but Too Slowly
-
 # @param {String} s
 # @return {Boolean}
 def check_valid_string(s)
     trimmed = trim(s)
-    if trimmed == ""
-        return true
-    end
-    count = trimmed.count('*')
-    if count == 0
-        return false
+    t_s = trimmed.size
+
+    l_index = trimmed.index('(')
+    r_index = trimmed.reverse.index(')')
+
+    result = true
+    if not l_index.nil?
+        s1 = trimmed[l_index..t_s]
+        result = analyze(s1)
+        if not r_index.nil?
+            s2 = trimmed[0..t_s-r_index-1]
+            result = result && analyze(s2.reverse)
+        end
+    elsif not r_index.nil?
+        s2 = trimmed[0..t_s-r_index-1]
+        result = analyze(s2.reverse)
     end
     
-    s_map = stars_map(trimmed)
-    substitute = trimmed.split('')
-    i = 3**count    #00 01 02 10 11 12 20 21 22
-    while i>0
-        i_cp = i
-        (0..count-1).each do |x|
-            bit = i_cp%3
-            i_cp/=3
-            substitute[s_map[x]] = i_to_val(bit)
-        end
-        final = trim(substitute.join(''))
-        if final == ""
-            return true
-        end
+    return result
+end
 
-        i-= 1
+def analyze(s)
+    stack = []
+    s.each_char do |c|
+        if c == '(' or c == ')'
+            stack.push(c)
+        else
+            stack.pop()
+        end
     end
-    return false
+    return stack.empty?
 end
 
 def trim(s)
     stack = []
     arr_format = s.split('')
-    index = 0
-    arr_format.each do |c|
+    arr_format.each_with_index do |c, i|
         if c == '('
-            stack.push([c,index])
+            stack.push(i)
         elsif c == ')'
             if not stack.empty?
-                _,leftmost_i = stack.pop()
-                arr_format[leftmost_i] = ''
-                arr_format[index] = ''
+                arr_format[stack.pop()] = ''
+                arr_format[i] = ''
             end
         end
-        index+=1
     end
     return arr_format.join('')
 end
 
-def stars_map(s)
-    s_map = []
-    index = 0
-    s.each_char do |c|
-        if c == '*'
-            s_map.push(index)
-        end
-        index+=1
-    end
-    return s_map
-end
+=begin
 
-def i_to_val(i)
-    if i == 0
-        return '('
-    elsif i == 1
-        return ')'
-    else
-        return ''
-    end
-end
-        
+TESTCASES
+
+"()"
+"*))"
+"(*"
+"*(*"
+"**((*"
+"**(((*(*(((((((*(*"
+"*()(())*()(()()((()(()()*)(*(())((((((((()*)(()(*)"
+")*"
+"*)*"
+"*)*))))))*)*)*))**"
+"()()((*()()(*()((())()))))(()())))(((()*())))))(())()))((*(())))))()))))())*(())()(()(*))*(*"
+"*))"
+"(*"
+"*))(*"
+
+=end
